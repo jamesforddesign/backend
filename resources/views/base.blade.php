11f4cda9c81430b2cb8a50995cadfe0dbba4c945
Pertti Soomann <psoomann@jfd.co.uk>
2025-12-17 14:48:28 +0000

    Update trigger code to match other usecases in the same view

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" media="all" href="/css/project.css">
     @yield('project-css')
     @include('nodes.backend::partials.favicon')
-    @env('staging')
+    @if (env('APP_ENV') == 'staging')
         <script>
             window.markerConfig = {
                 project: '6942bc687ee72c01c9fcaf5d',
@@ -20,7 +20,7 @@
 
             !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","clearReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
         </script>
-    @endenv
+    @endif
 </head>
 
 <body class="{{ env('APP_ENV') }} fullbleed">
