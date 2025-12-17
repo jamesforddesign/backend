f4cceb242fe249a5512b14137dcd03275426d375
Pertti Soomann <psoomann@jfd.co.uk>
2025-12-17 14:30:18 +0000

    Add marker IO

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -11,6 +11,16 @@
     <link rel="stylesheet" media="all" href="/css/project.css">
     @yield('project-css')
     @include('nodes.backend::partials.favicon')
+    @env('staging')
+        <script>
+            window.markerConfig = {
+                project: '6942bc687ee72c01c9fcaf5d',
+                source: 'snippet'
+            };
+
+            !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","clearReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
+        </script>
+    @endenv
 </head>
 
 <body class="{{ env('APP_ENV') }} fullbleed">
